4272b7d8cb284e70812bac3047fc01942602f44e
LuisFernandoGonzalezCortes <88851274+LuisFernandoGonzalezCortes@users.noreply.github.com>
2021-08-25 22:41:04 -0500

    Update exercise.py

--- assignments/01IdentificaSigno/src/exercise.py
+++ assignments/01IdentificaSigno/src/exercise.py
@@ -1,8 +1,17 @@
 import math
 def main():
     #escribe tu código abajo de esta línea
-    pass
     
+    x=int(input("Dame un numero: "))
+
+    if (x>0):
+        print ("Es positivo")
+
+    elif (x<0):
+        print ("Es negativo")
+
+    else:
+        print ("Es cero")
 
 if __name__ == '__main__':
     main()
