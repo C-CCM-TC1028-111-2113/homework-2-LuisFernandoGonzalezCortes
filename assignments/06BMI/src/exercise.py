7b185b24af2bc0f6024abf77c41d717d9c358a13
LuisFernandoGonzalezCortes <88851274+LuisFernandoGonzalezCortes@users.noreply.github.com>
2021-08-26 00:01:39 -0500

    Update exercise.py

--- assignments/06BMI/src/exercise.py
+++ assignments/06BMI/src/exercise.py
@@ -1,5 +1,25 @@
 def main():
-    #escribe tu código abajo de esta línea
-    pass
-if __name__=='__main__':
+    # Escribe el código adecuado para completar el programa
+    peso = float(input("Peso en kg: "))
+    altura = float(input("Altura en m: "))
+
+    k=(peso) / (altura * altura)
+
+    if (k < 20):
+        print ("PESO BAJO")
+
+    elif (20 <= k < 25):
+        print ("NORMAL")
+
+    elif (25 <= k < 30):
+        print ("SOBREPESO")
+
+    elif (30 <= k < 40):
+        print ("OBESIDAD")
+
+    elif (k >= 40):
+        print ("OBESIDAD MORBIDA")
+    
+
+if __name__ == '__main__':
     main()
